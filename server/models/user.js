d85ca112215cf041a37839fc1b6b27d1a5fccae3
Amahnui lordz <amahnuilordz@gmail.com>
2018-06-12 03:16:23 +0100

    adding login route

--- server/models/user.js
+++ server/models/user.js
@@ -67,6 +67,24 @@ return User.findOne({
 });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var user = this;
+  return User.findOne({email}).then((user) =>{
+    if(!user){
+      return Promise.reject();
+    }
+    return new Promise((resolve, reject) =>{
+      bcrypt.compare(password, user.password , (err, res ) =>{
+        if (true){
+          resolve(user);
+        } else{
+          return promise.reject();
+        }
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   var user = this;
   if (user.isModified('password')) {
